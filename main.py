239ce6047f89c9fec38c85e3841efc9919732b39
afidzulinn <afidzulin89@gmail.com>
2024-08-21 11:47:33 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from tensorflow.keras.models import load_model
+import numpy as np
+import io
+import uvicorn
+from PIL import Image
+
+from utils.processing import prepare_image
+
+app = FastAPI()
+
+# Load the model
+model = load_model("model_acc_97.h5")
+
+# define class name
+class_names = ["bakso", "gado", "gudeg", "rendang", "sate"]
+
+
+@app.get("/")
+async def home():
+    return {"message": "API Predict Makanan Bakso, Rendang, Gado - gado, Sate, Gudeg"}
+
+@app.post("/predict-image")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents))
+    img_array = prepare_image(img)
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+
+    return JSONResponse({"prediction": predicted_class})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9786) # ini port
---
+++ utils/processing.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+
+def prepare_image(img: Image.Image, target_size=(200, 200)):
+    img = img.resize(target_size)
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+    return img_array
